7748f3d0bb12138f77522b98e6dfe9170c19b3f8
Nick <nbeirne@users.noreply.github.com>
2024-03-17 15:05:18 -0400

    Add json

---
+++ index.js
@@ -0,0 +1,57 @@
+const arr = [
+  {id: 1, name: 'Alice', age: 29},
+  {id: 2, name: 'Bobby Hadz', age: 30},
+  {id: 3, name: 'Carl', age: 31},
+];
+
+async function getPrompts() {
+  const response = await fetch(
+    './prompts.json',
+    {
+      mode: 'cors',
+      method: 'GET',
+    },
+  );
+  if (!response.ok) {
+    throw new Error(`Error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+}
+const prompts = getPrompts()
+
+function newPrompt() {
+  prompts.then(data => {
+    var item = data[Math.floor(Math.random()*data.length)];
+
+
+    const preElement = document.getElementById('prompt-data');
+    preElement.innerHTML = item.text;
+  })
+}
+
+newPrompt()
+
+/*
+function createListItem(item) {
+  const li = document.createElement('li');
+  li.innerHTML = item.text;
+  return li;
+}
+
+prompts.then(data => {
+  const preElement = document.getElementById('prompt-data');
+
+  //preElement.style.fontSize = '18px';
+
+  const ch = data.map(createListItem);
+  const ul = document.createElement('ul');
+
+  ch.forEach(child => {
+    ul.appendChild(child);
+  });
+  
+  preElement.appendChild(ul);
+})
+*/
